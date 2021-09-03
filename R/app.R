198eb916a767b8a47f38d5c54dca69436e4af6c4
sara-vanerp <sara.vanerp@gmail.com>
2021-09-03 10:34:55 +0200

    added parameter types

--- R/app.R
+++ R/app.R
@@ -3,7 +3,6 @@
 
 library(shiny)
 library(lavaan)
-library(blavaan)
 library(tidySEM)
 library(plotly)
 
@@ -27,11 +26,11 @@ ui <- fluidPage(
                                      Semicolon = ";",
                                      Tab = "\t"),
                          selected = ","),
-            
+
             tags$hr(),
-            
-            # specify model 
-            textAreaInput("model", p("Specify the model using", a("lavaan syntax.", href = "https://lavaan.ugent.be/index.html")), 
+
+            # specify model
+            textAreaInput("model", p("Specify the model using", a("lavaan syntax.", href = "https://lavaan.ugent.be/index.html")),
                           value = "
                           # latent variable definitions
                             ind60 =~ x1 + x2 + x3
@@ -47,7 +46,7 @@ ui <- fluidPage(
                             y4 ~~ y8
                             y6 ~~ y8 ",
                            rows = 15),
-            
+
             # plotting options
             selectInput("layout", "Which layout to use for the model plot?",
                          choices = c("Tree" = "layout_as_tree",
@@ -58,50 +57,63 @@ ui <- fluidPage(
                                      "Random" = "layout_randomly"),
                          selected = "layout_as_tree"),
         ),
-        
+
         mainPanel(
             plotlyOutput("mod.plot"),
-            
-            verbatimTextOutput("clicks")
+
+            textOutput("info")
         )
     )
 )
 
 
 server <- function(input, output) {
-    
+
     # read data file
     dat <- reactive({
         req(input$file)
-        
+
         read.csv(input$file$datapath,
                  sep = input$sep)
     })
-    
+
     # add labels to the model and fit using lavaan
     fit <- reactive({
         req(input$model)
-        
+
         mod.lbl <- label_syntax_fun(toString(input$model))
         #mod.fit <- sem(mod.lbl, data = dat())
         mod.fit <- sem(mod.lbl, data = PoliticalDemocracy) #TODO: use input data instead of placeholder
     })
-    
+
     # plot the model
+    # Note: custom layout not working yet
     output$mod.plot <- renderPlotly({
         ggplotly(plot_fun(fit(), layout = input$layout), tooltip = "text")
     })
-    
+
     click_data <- reactive({
         click <- event_data("plotly_click")$customdata
     })
-    
+
     output$clicks <- renderPrint({
         data.frame(click_data())
     })
-    
-    
-    
+
+    output$info <- renderText({
+        if(is.null(click_data())){
+            print("Please select a parameter in the model by clicking on one of the labels")
+        } else if(grepl("l", click_data()) == TRUE){ # loadings
+            print("This is a loading")
+        } else if(grepl("v", click_data()) == TRUE){ # variances
+            print("This is a variance")
+        } else if(grepl("r", click_data()) == TRUE){ # correlations
+            print("This is a correlation")
+        } else if(grepl("b", click_data()) == TRUE){ # structural regression parameters
+            print("This is a structural regression parameter")
+        }
+    })
+
 }
 
 shinyApp(ui = ui, server = server)
